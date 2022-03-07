dea28831fb2ade86b6b96716fc2e109f9e5d4a9f
GSX <1019232598@qq.com>
2022-03-07 10:02:50 +0800

    整理c语言算法文件夹

--- TyporaPRO/ArithmeticAndStructure/C/Code/01_LinearList/Test.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by GanShaoXi on 2022/3/4.
-//
-#include <stdio.h>
-
-int main(){
-    printf("%s\n","hello world");
-    return 0;
-}
--- TyporaPRO/ArithmeticAndStructure/C/Code/01_LinearList/cmake-build-debug/CMakeFiles/SequenceList.c.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SequenceList.c.
-# This may be replaced when dependencies are built.
